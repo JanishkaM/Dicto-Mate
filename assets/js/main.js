27e2a73dc32d75dc8812ee041fe69fa9b18fc172
Janishka Madushan <106330319+JanishkaM@users.noreply.github.com>
2023-05-17 23:32:33 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-const header = document.querySelector('header')
 const footer = document.querySelector('footer')
 const preloader = document.querySelector(".preloader")
 
@@ -21,54 +20,6 @@ window.onload = function(){
   //hide the preloader
   preloader.style.display = "none";
 }
-
-
-header.innerHTML = 
-`
-<nav class="des-nav navbar navbar-expand-md d-none d-md-block navbar-dark bg-primary">
-  <div class="container">
-    <a class="navbar-brand" href="index.html"><div class="logo">Dicto Mate</div></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.html">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="dicto.html">Dicto</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="dictionary.html">Dictionary</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="https://voicytext.netlify.app/" target="_blank">Voicy</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contact.html">Feedback</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<nav class="mob-nav d-block d-md-none">
-        <ul class="nav justify-content-around position-fixed">
-            <li class="nav-item">
-              <a class="nav-link active p-0" aria-current="page" href="index.html"><i class="bi bi-house"></i></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link p-0" href="dicto.html"><i class="bi bi-pencil-square"></i></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link p-0" href="dictionary.html"><i class="bi bi-journal-text"></i></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link p-0" href="contact.html" ><i class="bi bi-envelope"></i></a>
-            </li>
-          </ul>
-    </nav>
-`
 
 footer.innerHTML += 
 `
